036a178d740b001afbf6591e076cf610f95eb15e
TedRomato <59472129+TedRomato@users.noreply.github.com>
2020-06-12 09:03:52 +0200

    Image set up

--- ANTS/src/gameObjectClasses/Tree.java
+++ ANTS/src/gameObjectClasses/Tree.java
@@ -18,11 +18,11 @@ public class Tree extends SolidObject implements Animated{
 		this.baseTile = baseTile;
 		
 		makeShape("0,0;-1,0;0,-1;1,0;0,1;-1,-1;1,1;1,-1;-1,1;-2,-1;-2,0;-2,1;2,-1;2,0;2,1;-1,2;0,2;1,2;-3,0;3,0;0,3;-1,-2;0,-2;1,-2;0,-3;");
-
-		
 		setOccupiedTiles(g);
 		
 		sprites = new BufferedImage[3];
+		setImageScale(18,4);
+		setImageOffset(-5.2, -11.5);
 		
 		try {
 			sprites[0] = ImageIO.read(new File("ANTS/src/TreeSprites/Tree1.png"));
