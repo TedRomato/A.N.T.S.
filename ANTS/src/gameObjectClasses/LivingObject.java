aaed786af19329253859d451983a666a3794ad55
TedRomato <vainerondrej@gmail.com>
2020-06-14 12:16:52 +0200

    Living ob rotate image done

--- ANTS/src/gameObjectClasses/LivingObject.java
+++ ANTS/src/gameObjectClasses/LivingObject.java
@@ -114,12 +114,25 @@ public abstract class LivingObject extends GameObject{
 		
 	}
 	
+	public static void rotateImage(Graphics2D g,BufferedImage img,int width, int height,double ra/*angle of rotation*/, Point rp, int rpX/*rotation point offset X*/, int rpY/*rotation point offset Y*/) {
+		AffineTransform trans = new AffineTransform();
+		trans.rotate(Math.toRadians(ra),(int)(rp.getX()/* +*camera scale*/),(int)(rp.getY()/* +*camera scale)*/));
+		AffineTransform old = g.getTransform();
+		g.transform(trans);
+		g.drawImage(img, (int)(rp.getX()-rpX/* +*camera scale)*/),(int)(rp.getY()-rpY/* +*camera scale)*/),width,height,null);
+		g.setTransform(old);
+	}
+	
 	public void rotateImage(Graphics2D g,Camera camera, double tileRatio) {
 		AffineTransform trans = new AffineTransform();
 		trans.rotate(angle,(int)(rp.getX()*tileRatio - camera.getX()),(int)(rp.getY()*tileRatio - camera.getY()));
 		AffineTransform old = g.getTransform();
 		g.transform(trans);
-		g.drawImage(sprites[currentSpritePointer], (int)(rp.getX()*tileRatio- camera.getX()- imageOffsetX*tileRatio),(int)(rp.getY()*tileRatio-camera.getY()-imageOffsetY*tileRatio),null);
+		g.drawImage(sprites[currentSpritePointer], 
+				(int)(rp.getX()*tileRatio- camera.getX()- imageOffsetX*tileRatio),
+				(int)(rp.getY()*tileRatio-camera.getY()-imageOffsetY*tileRatio),
+				(int)(imageWidth*tileRatio),
+				(int)(imageHeight*tileRatio),null);
 		g.setTransform(old);
 	}
 	
